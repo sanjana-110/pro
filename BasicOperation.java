fa02197a66345e8524bd2f31893bac2c89662739
Apoorva-ry <apoorvayaligar33@gmail.com>
2025-05-06 16:52:25 +0530

    Operation

---
+++ BasicOperation.java
@@ -0,0 +1,16 @@
+package operation;
+
+public class BasicOperation {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+        int diff = a-b;
+        int prod = a*b;
+
+        System.out.println("sum:" +sum);
+        System.out.println("diff:" +diff);
+        System.out.println("prod:" +prod);
+    }
+}
